66ecb921a867a92c2adabcb2f95bfb926b1d09b8
Shirokun <khoirul@radyalabs.id>
2023-09-26 16:27:14 +0700

    Initial Commit

---
+++ footballui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  footballui
+//
+//  Created by macbook on 26/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ footballui/footballuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  footballuiApp.swift
+//  footballui
+//
+//  Created by macbook on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct footballuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
